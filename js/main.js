49a779d281510e68966258b1521dfc8bcb1736ff
Tyler Miles <tylrmls@gmail.com>
2017-07-13 18:21:20 -0600

    edited main.js to add and resize iframes

--- js/main.js
+++ js/main.js
@@ -3,21 +3,22 @@ var favSongs = [
     order: 1,
     song: "No Quarter",
     artist: "Led Zepplin",
-    link: `<iframe width="560" height="315" src="https://www.youtube.com/embed/Pu94mWlgzMY" frameborder="0" allowfullscreen></iframe>`
+    link: "<iframe id=\"youtubeVideos_0\" src=\"https://www.youtube.com/embed/Pu94mWlgzMY\" frameborder=\"0\" allowfullscreen></iframe>"
   },
   {
     order: 2,
     song: "Wish You Were Here",
     artist: "Pink Floyd",
-    link: `<iframe width="560" height="315" src="https://www.youtube.com/embed/hjpF8ukSrvk" frameborder="0" allowfullscreen></iframe>`
+    link: "<iframe id=\"youtubeVideos_1\" src=\"https://www.youtube.com/embed/hjpF8ukSrvk\" frameborder=\"0\" allowfullscreen></iframe>"
   },
   {
     order: 3,
     song: "Bohemian Rhapsody",
     artist: "Queen",
-    link: `<iframe width="560" height="315" src="https://www.youtube.com/embed/fJ9rUzIMcZQ" frameborder="0" allowfullscreen></iframe>`
+    link: "<iframe id=\"youtubeVideos_2\" src=\"https://www.youtube.com/embed/fJ9rUzIMcZQ\" frameborder=\"0\" allowfullscreen></iframe>"
   }
 ];
+var videosLoaded = 0;
 
 console.log(favSongs);
 var tbody = document.querySelector("tbody");
@@ -36,7 +37,19 @@ for(var i in favSongs) {
       tr.appendChild(td1);
   }
   tbody.appendChild(tr);
+  try {
+    document.querySelector("#youtubeVideos_" + videosLoaded).style.width = "450px";
+    document.querySelector("#youtubeVideos_" + videosLoaded).style.height = "195px";
+    videosLoaded++;
+  } catch(error) {
+    console.log("Error resizing iframes: " + error);
+  }
 }
+
+if(videosLoaded == favSongs.length) {
+  console.log("All Iframes Loaded");
+}
+
 /*tbody.addEventListener('click', function() {
   tbody.style.color = '#5760E0';
 }); */
